a3ad11b9c79cf2f19f5cc86155f4249ba1e1113c
Konbil William Chol Deng <58990246+konbil@users.noreply.github.com>
2022-08-23 18:24:35 +0300

    more

--- Arrays/src/com/konbil/Main.java
+++ Arrays/src/com/konbil/Main.java
@@ -1,11 +1,24 @@
 package com.konbil;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-   //syntax
-        int[] no = new int[5]; //creates the object in heap memory
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5]; //creates the object in heap memory
         int[] no1 = {1,34,9,2,2};
+
+        //Input using for loops
+        System.out.print("Please enter the numbers:" );
+        for(int i=0; i<arr.length; i++) {
+
+          arr[i] = in.nextInt();
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + "  " );
+        }
     }
 }
