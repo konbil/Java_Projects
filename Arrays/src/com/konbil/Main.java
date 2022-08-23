ae0cf7d5a2e95894e30a00040a0dc6ce9ccb53ba
Konbil William Chol Deng <58990246+konbil@users.noreply.github.com>
2022-08-23 17:10:18 +0300

    Arrays
    arrays

---
+++ Arrays/src/com/konbil/Main.java
@@ -0,0 +1,11 @@
+package com.konbil;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+   //syntax
+        int[] no = new int[5]; //creates the object in heap memory
+        int[] no1 = {1,34,9,2,2};
+    }
+}
