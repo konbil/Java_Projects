58faf996f0b723612535baed8a332be078b0ff3a
Konbil William Chol Deng <58990246+konbil@users.noreply.github.com>
2022-08-29 17:03:43 +0300

    Update OrderAgnosticsBS.java

--- Binarysearch/src/com/konbil/OrderAgnosticsBS.java
+++ Binarysearch/src/com/konbil/OrderAgnosticsBS.java
@@ -1,5 +1,11 @@
 package com.konbil;
 
+//why binary search??
+
+//Binary search is an efficient algorithm for finding an item from a sorted list of items.
+//It works by repeatedly dividing in half the portion of the list that could contain the item,
+// until you've narrowed down the possible locations to just one.
+
 public class OrderAgnosticsBS {
     public static void main(String[] args) {
         //int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45};
