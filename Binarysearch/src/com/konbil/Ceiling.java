647b7f2a21d26207cf36de460689342330729733
Konbil William Chol Deng <58990246+konbil@users.noreply.github.com>
2022-08-30 01:28:23 +0300

    more bs
    bs

--- Binarysearch/src/com/konbil/Ceiling.java
+++ Binarysearch/src/com/konbil/Ceiling.java
@@ -9,39 +9,27 @@ public class Ceiling {
 
     }
     static int CeilingBs(int arr[], int target){
+
+        // but what if the target is greater than the greatest number in the array
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
         int start = 0;
         int end = arr.length - 1;
-        boolean isAsc = arr[start] < arr[end];
 
-
-        while (start <= end) {
-//            find the middle element
-//            int = (start + end) / 2;//might be possible that (start +end) exceeds the range of int in java
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
-            if (arr[mid] == target) {
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
                 return mid;
             }
-
-            if (isAsc) {
-                if (target < arr[mid]) {
-                    end = mid - 1;
-
-                } else {
-                    start = mid + 1;
-                }
-
-            }
-
-            else {
-                if (target > mid) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-
-
         }
         return arr[start];
     }
--- Binarysearch/src/com/konbil/FloorNumber.java
+++ Binarysearch/src/com/konbil/FloorNumber.java
@@ -11,37 +11,20 @@ public class FloorNumber {
     static int FloorBs(int arr[], int target){
         int start = 0;
         int end = arr.length - 1;
-        boolean isAsc = arr[start] < arr[end];
 
-
-        while (start <= end) {
-//            find the middle element
-//            int = (start + end) / 2;//might be possible that (start +end) exceeds the range of int in java
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
-            if (arr[mid] == target) {
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
                 return mid;
             }
-
-            if (isAsc) {
-                if (target < arr[mid]) {
-                    end = mid - 1;
-
-                } else {
-                    start = mid + 1;
-                }
-
-            }
-
-            else {
-                if (target > mid) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-
-
         }
         return arr[end];
     }
